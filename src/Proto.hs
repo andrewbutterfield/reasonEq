adca146f2d6e4a4aeb4cf8e403de4d515ae306f5
Andrew Butterfield <andrew.butterfield@scss.tcd.ie>
2017-10-17 17:53:34 +0200

    working to prototype acyclic checking with timings

--- src/Proto.hs
+++ src/Proto.hs
@@ -1,76 +1,157 @@
-{-# LANGUAGE PatternSynonyms #-}
-module Proto
-    ( someProto
-    , pattern F
-    , Test(K,V,L)
-    , dbl )
-where
+{- =============================================================================
+ We explore different ways to ensure that a map (a -> Set a)
+ is acyclic.
 
-import Control.Monad
+ We use Data.Set and Data.Map rather than lists
+ as these already have some efficiencies.
+-}
+module Proto where
 
-type Embed = Double
+import Data.Set (Set)
+import qualified Data.Set as S
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Time
 
-embed :: Int -> Double
-embed = fromInteger . toInteger
+type Rel a = Map a (Set a)
 
-minInt = -2^31 :: Int
-maxInt = 2^31-1 :: Int
+{- -----------------------------------------------------------------------------
 
-extract :: Double -> Int
-extract = round
+ Introduction
 
-someProto :: IO ()
-someProto
- = do let eyes = [minInt,minInt+1,minInt+10,-10,-1,0,1,10,maxInt-10,maxInt-1,maxInt] :: [Int]
-      putStrLn ("eyes  = " ++ show eyes)
-      let ds = map embed eyes
-      putStrLn ("ds   = " ++ show ds)
-      let is' = map extract ds
-      putStrLn ("is'   = " ++ show is')
+ We consider how we might ensure that the operation
+ insert a bs m  does not result in a cycle.
+ We want that it has good performance.
+ In effect we want the insert operation to visibly fail
+ if it would result in a cycle.
+-}
+type InsertOp a = a -> Set a -> Rel a -> Maybe (Rel a)
 
-someProto'
- = do let n = A (L "x" $ K (100/3)) (V "𝜔")
-      let t = A (A (A n n) (A n n)) (A (A n n) (A n n))
-      putStrLn ("t  = " ++ show t)
-      save t
-      t' <- restore
-      putStrLn ("t' = " ++ show t')
-      putStrLn ("t==t' is " ++ show (t==t'))
+{- -----------------------------------------------------------------------------
 
-data Test
- = K Double
- | V String
- | A Test Test
- | L String Test
- deriving (Eq,Ord,Show,Read)
+ Approach 0
 
-pattern F s t = A (V s) t
+ An insertion that does no checks, for speed reference,
+ as well as one that doesn't insert!
 
-dbl (F "2" tst) = A tst tst
-dbl tst = tst
+-}
+ins0 :: Ord a => InsertOp a
+ins0 a bs m  =   return $ M.insertWith (S.union) a bs m
 
-lshow = unlines . lshow'
+insx :: Ord a => InsertOp a
+insx a bs m = return m
 
-lshow' (K d) = ["(K "++show d++")"]
-lshow' (V s) = ["(V "++show s++")"]
-lshow' (A t1 t2)
- = ["(A "] ++ lshow' t1 ++ lshow' t2 ++ [")"]
-lshow' (L v t)
- = ["(L "++ show v] ++ lshow' t ++ [")"]
+{- -----------------------------------------------------------------------------
 
-save :: Test -> IO ()
-save t = writeFile "test.sav" $ lshow t
+ Approach 1
 
-restore :: IO Test
-restore = fmap read $ readFile "test.sav"
+ Do the insert, then check result for cycles.
+ We will explore two cycle-check algorithms
+ (transitive closure vs. annihilation),
+ so we make that algorithm a parameter
 
-likeEvens :: MonadPlus mp => Int -> mp Int
-likeEvens 0 = return 0
-likeEvens i
- | odd i = fail "Don't like odd numbers :-("
- | otherwise  = return i `mplus` likeEvens (i `div` 2)
+-}
+type CycChk a = Rel a -> Bool
 
-fail2 :: MonadPlus mp => mp Int
-fail2 = (return 42 >> fail "Ouch!")
-        `mplus`
-        (fail "Oof!" >> return 24)
+ins1 :: Ord a => CycChk a -> InsertOp a
+ins1 chk a bs m
+ | chk m'  =  return m'
+ | otherwise  =  fail "ins1: cycle!"
+ where m' = M.insertWith (S.union) a bs m
+
+{- -----------------------------------------------------------------------------
+
+ Approach 2
+
+ Check each insertion on the fly.
+
+-}
+ins2 :: Ord a => InsertOp a
+ins2 a bs m  =  error "ins2: NYI"
+
+
+{- -----------------------------------------------------------------------------
+
+ Performance
+
+ We want a big test that builds a big map with failures along the way
+ so we can get good benchmarks
+
+-}
+type AddTst a = (a, Set a)
+
+type Test a = [AddTst a] -> InsertOp a -> Rel a
+
+runtest :: Ord a => Test a
+runtest tests ins  = run ins M.empty $! tests
+
+run ins m [] = m
+run ins m ((a,bs):tests)
+ = case ins a bs m of
+     Nothing  ->  run ins m tests
+     Just m'  ->  run ins m' tests
+
+{- ----------
+
+    Test generator
+
+    Want to generate a lot of tests with a significant proportion of bad inserts
+    that has a large final result.
+-}
+
+-- some quick shorthands
+emp = S.empty
+sngl = S.singleton
+set :: Ord a => [a] -> Set a
+set = S.fromList
+
+{-
+   auto-generate, with increasing size s = 0..n
+
+-}
+lsqr :: Ord a => [a] -> [(a,Set a)]
+lsqr as = concat $ map (tag as) as
+ where tag as a = map (pair a) as
+       pair a b = (a,sngl b)
+
+lslide :: Ord a => [a] -> [(a,Set a)]
+lslide as = lslide' [] as
+ where
+   lslide' _ [] = []
+   lslide' as (b:cs) = (b,set (as++cs)) : lslide' (b:as) cs
+
+auto n
+  = concat $ map gen [0..n]
+  where
+    gen i
+      = lsqr ilist ++ lslide ilist
+      where ilist = reverse [0..i]
+
+
+{- ----------
+
+   Test Harness
+
+   We take a test and list of inserts and run them with timing.
+
+-}
+timedtests inss tests
+ = mapM_ (dotimedtest tests) inss
+ where
+    dotimedtest tests ins
+      = do start <- getCurrentTime
+           return $! runtest tests ins
+           stop <- getCurrentTime
+           print $ diffUTCTime stop start
+
+
+
+{-
+Import Data.Time
+...
+   start <- getCurrentTime
+   runOperation
+   stop <- getCurrentTime
+   print $ diffUTCTime stop start
+
+-}

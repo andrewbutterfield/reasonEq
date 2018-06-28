93e0ffdf36a65cf004f7c758618375326d4e3427
Andrew Butterfield <Andrew.Butterfield@scss.tcd.ie>
2018-06-28 14:04:26 +0100

    looking at a list of lists of tuples implementation

--- proto/Stratfied.hs
+++ proto/Stratfied.hs
@@ -32,3 +32,55 @@ insDAG n ns dag
  | n `M.member` dag  =  fail "domain node already present"
  | ns `S.isSubsetOf` M.keysSet dag  =  return $ M.insert n ns dag
  | otherwise  =  fail "some range nodes not present"
+
+
+{- Better idea: stratified lists
+      3     [ [ (3,[1,2]) ]
+     / \    ,
+     1 2      [ (1,[0]), (2,[0]) ]
+     \/     ,
+     0        [ (0,[]) ] ]
+-}
+
+type SDAGEntry a  =  (a,[a])
+type SDAGLevel a  =  [SDAGEntry a]
+type SDAG a       =  [SDAGLevel a]
+
+lvlDom :: SDAGLevel a -> [a]
+lvlDom lvl = map fst lvl
+
+lvlRng :: SDAGLevel a -> [a]
+lvlRng lvl = concat $ map snd lvl
+
+issubset :: Eq a => [a] -> [a] -> Bool
+xs `issubset` ys  =  null (xs \\ ys)
+
+inLvlDom :: Eq a => a -> SDAGLevel a -> Bool
+n `inLvlDom` level = n `elem` lvlDom level
+
+withinLvlDom :: Eq a => [a] -> SDAGLevel a -> Bool
+ns `withinLvlDom` level = ns `issubset` lvlDom level
+
+withinLvlRng :: Eq a => [a] -> SDAGLevel a -> Bool
+ns `withinLvlRng` level = ns `issubset` lvlRng level
+
+sdagDOM :: SDAG a -> [a]
+sdagDOM sdag = concat $ map lvlDom sdag
+
+inSDAGDom :: Eq a => a -> SDAG a -> Bool
+n `inSDAGDom` sdag = n `elem`  sdagDOM sdag
+
+insSDAG :: (Eq a, Monad m) => a -> [a] -> SDAG a -> m (SDAG a)
+
+insSDAG n [] []  = return [ [ (n,[]) ] ]
+insSDAG n ns []  =  fail "no range nodes present"
+
+insSDAG n [] [level0]
+ | n `inLvlDom` level0  =  fail "domain node already present"
+ | otherwise            =  return [(n,[]):level0]
+insSDAG n ns [level0]
+ | n `inLvlDom` level0  =  fail "domain node already present"
+ | ns `withinLvlDom` level0 = return [ [ (n,ns)], level0 ]
+ | otherwise  =  fail "some range nodes not present"
+
+insSDAG n ns (lvl2:lvl1:lvls) = error "not sure what to do here!"

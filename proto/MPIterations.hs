83df659f9a9892c947135997a462d23734d5c889
Andrew Butterfield <andrew.butterfield@scss.tcd.ie>
2019-02-22 20:52:43 +0100

    got cross-matching working as a prototype

--- proto/MPIterations.hs
+++ proto/MPIterations.hs
@@ -9,28 +9,52 @@ import Data.Map(Map)
 import qualified Data.Map as M
 import Control.Monad
 
+-- Reboot - we have  f :: MonadPlus mp => c -> p -> b -> mp b
+-- where b' is (probably) b with extra stuff
+type BasicF mp b c p = c -> p -> b -> mp b
 
-type BMatch mp c p b = c -> p -> b -> mp b
+type Combine b c b' = b -> [c] -> [c] -> b'
+
+manyToOne :: MonadPlus mp
+           => BasicF mp b c p
+           -> Combine b c b'
+           -> [c] -> p -> b
+           -> mp b'
+manyToOne bf cf cs p b = manyToOne' bf cf [] p b cs
+
+manyToOne' bf cf sc p b0 [] = fail "no candidates"
+manyToOne' bf cf sc p b0 (c:cs)
+  = (do b <- bf c p b0
+        return $ cf b sc cs)
+    `mplus`
+    manyToOne' bf cf (c:sc) p b0 cs
+
+manyToMany :: MonadPlus mp
+           => BasicF mp b c p
+           -> Combine b c b'
+           -> [c] -> [p] -> b
+           -> mp b'
+manyToMany bf cf cs ps b
+ = foldr mplus (fail ".") $ map f ps
+ where
+   f p = manyToOne bf cf cs p b
+
+defCombine :: Combine b c (b,[c])
+defCombine b sc cs = (b, reverse sc ++ cs)
+
+-- concretise...
 
 type Bind = Map Int Int
 
 -- match if candidate arity same as pattern
-mArity :: MonadPlus mp => BMatch mp Int Int Bind
+mArity :: MonadPlus mp => BasicF mp Bind Int Int
 mArity iC iP bind
  | even iC && even iP = return $ M.insert iP iC bind
  | odd iC && odd iP   = return $ M.insert iP iC bind
  | otherwise = fail "mArity differs"
 
-mwwe :: MonadPlus mp
-     => (BMatch mp c p b) -> [c] -> p -> b
-     -> mp (b,[c])
-mwwe m cs p b = mwwe' m [] p b cs
+manyToOneArity :: MonadPlus mp => [Int] -> Int -> Bind -> mp (Bind, [Int])
+manyToOneArity = manyToOne mArity defCombine
 
-mwwe' m sc p b [c]
- = do b' <- m c p b
-      return (b',reverse sc)
-mwwe' m sc p b (c:cs)
-  = (do b' <- m c p b
-        return (b',reverse sc++cs))
-    `mplus`
-    mwwe' m (c:sc) p b cs
+manyToManyArity :: MonadPlus mp => [Int] -> [Int] -> Bind -> mp (Bind, [Int])
+manyToManyArity = manyToMany mArity defCombine

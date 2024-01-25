a014b8e98ed99de72b68d1a49c8f25eaee4f1358
Andrew Butterfield <Andrew.Butterfield@scss.tcd.ie>
2024-01-25 18:44:28 +0000

    precedence works, working on fixity

--- proto/Precedence.hs
+++ proto/Precedence.hs
@@ -7,22 +7,46 @@ data Exp
  | C Char [Exp]
  deriving (Eq,Ord,Show)
 
+data Fix 
+  = N -- none
+  | L -- left-assoc
+  | R -- right-assoc
+  deriving (Eq,Ord,Show)
+
 data OpData
  = OD { op :: Char
       , prec :: Int
-      , isAssoc :: Bool
-      , isLAssoc :: Bool
-      , isRAssoc :: Bool
-      , isSymm :: Bool
+      , fixity :: Fix
       }
   deriving (Eq,Ord,Show)
 
 
+-- preludeFixities = concat
+--     [infixr_ 9  ["."]
+--     ,infixl_ 9  ["!!"]
+--     ,infixr_ 8  ["^","^^","**"]
+--     ,infixl_ 7  ["*","/","`quot`","`rem`","`div`","`mod`"]
+--     ,infixl_ 6  ["+","-"]
+--     ,infixr_ 5  [":","++"]
+--     ,infix_  4  ["==","/=","<","<=",">=",">","`elem`","`notElem`"]
+--     ,infixl_ 4  ["<$>","<$","<*>","<*","*>"]
+--     ,infixr_ 3  ["&&"]
+--     ,infixr_ 2  ["||"]
+--     ,infixl_ 1  [">>",">>="]
+--     ,infixr_ 1  ["=<<"]
+--     ,infixr_ 0  ["$","$!","`seq`"]
+--     ]
+
+-- operator precedences are always > 0
+-- top-level or just inside parentheses is = 0
 opdata
-  = [ OD '+' 6 False True  False True  -- infixl, symm
-    , OD '-' 6 False True  False False -- infixl, not-symm
-    , OD '*' 7 False True  False True  -- infixl, symm
-    , OD '/' 7 False True  False False -- infixl, not-symm
+  = [ OD '=' 4 N
+    , OD ':' 5 L
+    , OD '+' 6 L
+    , OD '-' 6 L
+    , OD '*' 7 L
+    , OD '/' 7 L
+    , OD '^' 8 R
     ]
 
 olkp :: [OpData] -> Char -> Maybe OpData
@@ -45,10 +69,17 @@ b = A 'b'
 c = A 'c'
 d = A 'd'
 
-add x y = C '+' [x,y]
-sub x y = C '-' [x,y]
-mul x y = C '*' [x,y]
-dvd x y = C '/' [x,y]
+add x y   =  C '+' [x,y]
+sub x y   =  C '-' [x,y]
+mul x y   =  C '*' [x,y]
+dvd x y   =  C '/' [x,y]
+eql x y   =  C '=' [x,y]
+cns x y   =  C ':' [x,y]
+app f es  
+  = case od f of
+     Just (OD _ _ N)
+       | length es /= 2  ->  C '?' (A f:es)
+     _                   ->  C f es
  
 pp :: Exp -> String
 pp exp = ppp 0 exp
@@ -56,5 +87,12 @@ pp exp = ppp 0 exp
 ppp p (A c) = [c]
 ppp p (C n es) 
   = case od n of
-      Nothing ->  n:"("++intercalate "," (map (ppp 0) es)++")"
-      Just od -> "od-nyi"
+      Nothing             ->  ppapp        n es
+      Just (OD _ p' N)    ->  ppinfix p p' n es
+      Just (OD _ p' fix)  ->  ppinfix p p' n es
+
+
+ppapp n es     =   n:"("++intercalate "," (map (ppp 0) es)++")"
+ppinfix p p' n es
+  | p' < p     =  "("++intercalate [' ',n,' '] (map (ppp 0)  es)++")"
+  | otherwise  =       intercalate [' ',n,' '] (map (ppp p') es)

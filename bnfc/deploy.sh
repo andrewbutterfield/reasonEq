483edccf24ee395ac0619629cdca10d284678835
Andrew Butterfield <Andrew.Butterfield@scss.tcd.ie>
2026-01-10 16:41:58 +0000

    adds Print.hs into src/REQ

--- bnfc/deploy.sh
+++ bnfc/deploy.sh
@@ -1,5 +1,7 @@
 cp REQ/Abs.hs  ../src/REQ/Abs.hs
 cp REQ/Lex.hs  ../src/REQ/Lex.hs
 cp REQ/Par.hs  ../src/REQ/Par.hs
+cp REQ/Print.hs  ../src/REQ/Print.hs
+echo "deployed to ../src/REQ:"
 ls ../src/REQ
 

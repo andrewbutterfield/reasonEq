aa8dfa04f87efdcb89448722a9428a0568355ff2
Andrew Butterfield <Andrew.Butterfield@scss.tcd.ie>
2026-04-22 17:41:22 +0100

    replace esc-sequence with unicode symbol

--- dump.hs
+++ dump.hs
@@ -1,7 +1,7 @@
-SSW 183 (SSA 1 "\8707") (SSA 0 "") 
-  ( SSL 182 (SSA 3 " \8226 ") 
-    [ SSA 4 "O$_1",SSL 175 (SSA 3 " \8743 ") 
-      [ SSL 27 (SSA 3 " \8838 ") 
+SSW 183 (SSA 1 "∃") (SSA 0 "") 
+  ( SSL 182 (SSA 3 " • ") 
+    [ SSA 4 "O$_1",SSL 175 (SSA 3 " ∧ ") 
+      [ SSL 27 (SSA 3 " ⊆ ") 
         [ SSL 12 (SSA 0 "") 
           [ SSA 2 "E1"
           , SSW 10 (SSA 1 "[") (SSA 1 "]") 
@@ -20,8 +20,8 @@ SSW 183 (SSA 1 "\8707") (SSA 0 "")
         ]
       , SSL 49 (SSA 3 " = ") 
         [ SSA 4 "ls_1"
-        , SSL 42 (SSA 3 " \8746 ") 
-            [ SSL 27 (SSA 3 " \8726 ") 
+        , SSL 42 (SSA 3 " ∪ ") 
+            [ SSL 27 (SSA 3 " ∖ ") 
               [ SSL 12 (SSA 0 "") 
                 [ SSA 2 "ls"
                 , SSW 10 (SSA 1 "[") (SSA 1 "]") 
@@ -43,8 +43,8 @@ SSW 183 (SSA 1 "\8707") (SSA 0 "")
       , SSS 81 (Colour '5') 
           ( SSL 81 (SSA 0 "") 
             [ SSA 1 "("
-            , SSL 79 (SSA 3 " \8743 ") 
-              [ SSL 18 (SSA 3 " \8838 ") 
+            , SSL 79 (SSA 3 " ∧ ") 
+              [ SSL 18 (SSA 3 " ⊆ ") 
                 [ SSL 11 (SSA 0 "") 
                   [ SSA 2 "E2"
                   , SSW 9 (SSA 1 "[") (SSA 1 "]") 
@@ -63,8 +63,8 @@ SSW 183 (SSA 1 "\8707") (SSA 0 "")
                   , SSW 9 (SSA 1 "[") (SSA 1 "]") 
                       ( SSL 7 (SSA 1 "/") [SSA 4 "O$_1",SSA 2 "O$"] )
                   ]
-                , SSL 32 (SSA 3 " \8746 ") 
-                  [ SSL 18 (SSA 3 " \8726 ") 
+                , SSL 32 (SSA 3 " ∪ ") 
+                  [ SSL 18 (SSA 3 " ∖ ") 
                     [ SSA 4 "ls_1",SSL 11 (SSA 0 "") 
                       [ SSA 2 "R2"
                       , SSW 9 (SSA 1 "[") (SSA 1 "]") 

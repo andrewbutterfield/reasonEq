8e783cdbb38c187f3531061a0989dc99cca77c7c
Andrew Butterfield <Andrew.Butterfield@scss.tcd.ie>
2026-04-22 17:34:37 +0100

    completes SS expansion of X_X_comp proof predicate

--- dump.hs
+++ dump.hs
@@ -22,7 +22,16 @@ SSW 183 (SSA 1 "\8707") (SSA 0 "")
         [ SSA 4 "ls_1"
         , SSL 42 (SSA 3 " \8746 ") 
             [ SSL 27 (SSA 3 " \8726 ") 
-              [ SSL 12 (SSA 0 "") [SSA 2 "ls",SSW 10 (SSA 1 "[") (SSA 1 "]") (SSL 8 (SSA 1 "/") [SSA 4 "O$_1",SSA 3 "O$'"])],SSL 12 (SSA 0 "") [SSA 2 "R1",SSW 10 (SSA 1 "[") (SSA 1 "]") (SSL 8 (SSA 1 "/") [SSA 4 "O$_1",SSA 3 "O$'"])]
+              [ SSL 12 (SSA 0 "") 
+                [ SSA 2 "ls"
+                , SSW 10 (SSA 1 "[") (SSA 1 "]") 
+                    ( SSL 8 (SSA 1 "/") [SSA 4 "O$_1",SSA 3 "O$'"] )
+                ]
+              , SSL 12 (SSA 0 "") 
+                [ SSA 2 "R1"
+                , SSW 10 (SSA 1 "[") (SSA 1 "]") 
+                    ( SSL 8 (SSA 1 "/") [SSA 4 "O$_1",SSA 3 "O$'"] )
+                ]
               ]
             ,SSL 12 (SSA 0 "") 
               [ SSA 2 "N1"
@@ -32,7 +41,46 @@ SSW 183 (SSA 1 "\8707") (SSA 0 "")
             ]
         ]
       , SSS 81 (Colour '5') 
-          ( SSL 81 (SSA 0 "") [SSA 1 "(",SSL 79 (SSA 3 " \8743 ") [SSL 18 (SSA 3 " \8838 ") [SSL 11 (SSA 0 "") [SSA 2 "E2",SSW 9 (SSA 1 "[") (SSA 1 "]") (SSL 7 (SSA 1 "/") [SSA 4 "O$_1",SSA 2 "O$"])],SSA 4 "ls_1"],SSL 8 (SSA 0 "") [SSA 1 "b",SSW 7 (SSA 1 "[") (SSA 1 "]") (SSL 5 (SSA 1 "/") [SSA 3 "s_1",SSA 1 "s"])],SSL 47 (SSA 3 " = ") [SSL 12 (SSA 0 "") [SSA 3 "ls'",SSW 9 (SSA 1 "[") (SSA 1 "]") (SSL 7 (SSA 1 "/") [SSA 4 "O$_1",SSA 2 "O$"])],SSL 32 (SSA 3 " \8746 ") [SSL 18 (SSA 3 " \8726 ") [SSA 4 "ls_1",SSL 11 (SSA 0 "") [SSA 2 "R2",SSW 9 (SSA 1 "[") (SSA 1 "]") (SSL 7 (SSA 1 "/") [SSA 4 "O$_1",SSA 2 "O$"])]],SSL 11 (SSA 0 "") [SSA 2 "N2",SSW 9 (SSA 1 "[") (SSA 1 "]") (SSL 7 (SSA 1 "/") [SSA 4 "O$_1",SSA 2 "O$"])]]]],SSA 1 ")"]
+          ( SSL 81 (SSA 0 "") 
+            [ SSA 1 "("
+            , SSL 79 (SSA 3 " \8743 ") 
+              [ SSL 18 (SSA 3 " \8838 ") 
+                [ SSL 11 (SSA 0 "") 
+                  [ SSA 2 "E2"
+                  , SSW 9 (SSA 1 "[") (SSA 1 "]") 
+                      ( SSL 7 (SSA 1 "/") [SSA 4 "O$_1",SSA 2 "O$"] )
+                  ]
+                , SSA 4 "ls_1"
+                ]
+              , SSL 8 (SSA 0 "") 
+                [ SSA 1 "b"
+                , SSW 7 (SSA 1 "[") (SSA 1 "]") 
+                    ( SSL 5 (SSA 1 "/") [SSA 3 "s_1",SSA 1 "s"] )
+                ]
+              , SSL 47 (SSA 3 " = ") 
+                [ SSL 12 (SSA 0 "") 
+                  [ SSA 3 "ls'"
+                  , SSW 9 (SSA 1 "[") (SSA 1 "]") 
+                      ( SSL 7 (SSA 1 "/") [SSA 4 "O$_1",SSA 2 "O$"] )
+                  ]
+                , SSL 32 (SSA 3 " \8746 ") 
+                  [ SSL 18 (SSA 3 " \8726 ") 
+                    [ SSA 4 "ls_1",SSL 11 (SSA 0 "") 
+                      [ SSA 2 "R2"
+                      , SSW 9 (SSA 1 "[") (SSA 1 "]") 
+                          ( SSL 7 (SSA 1 "/") [SSA 4 "O$_1",SSA 2 "O$"] )
+                      ]
+                    ]
+                  , SSL 11 (SSA 0 "") 
+                    [ SSA 2 "N2"
+                    , SSW 9 (SSA 1 "[") (SSA 1 "]") 
+                        ( SSL 7 (SSA 1 "/") [SSA 4 "O$_1",SSA 2 "O$"] )
+                    ]
+                  ]
+                ]
+              ]
+            , SSA 1 ")"
+            ]
           )
       ]
     ]
